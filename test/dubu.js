6acf853ec4c4f4b5b39bb5978e85e13c1fa6a708
kozaz <kozazz@hanmail.net>
2015-07-21 17:18:08 +0900

    qunit test

---
+++ test/dubu.js
@@ -0,0 +1,22 @@
+//QUnit.test( "hello test", function( assert ) {
+//    assert.ok( 1 == "1", "Passed!" );
+//});
+
+(function() {
+    //alert('adfs');
+    var _ = typeof require == 'function' ? require('..') : window._;
+
+    QUnit.module('Collections');
+
+    test('each', function() {
+        _.each([1, 2, 3], function(num, i) {
+            equal(num, i + 1, 'each iterators provide value and iteration count');
+        });
+
+    });
+
+    QUnit.test( "hello test", function( assert ) {
+        assert.ok( 1 == "1", "Passed!" );
+    });
+
+}());
